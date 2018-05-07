4a9cfe46696f107faa22119af4cf7546b539aa3a
Patryk Chelmecki <pat.chelmecki@gmail.com>
2018-05-07 16:40:56 +0200

    created render and physics/logic loops

--- core/src/com/CheapCleaningAndCleaning/CheapCleaningAndCleaning.java
+++ core/src/com/CheapCleaningAndCleaning/CheapCleaningAndCleaning.java
@@ -7,21 +7,37 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class CheapCleaningAndCleaning extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+    SpriteBatch batch;
+    Texture img;
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        img = new Texture("badlogic.jpg");
+    }
+
+    private long time = 0;
+    private long currentSystemTime = System.nanoTime();
+    private long accumulator = 0;
+
+    @Override
+    public void render() {
+        long newTime = System.nanoTime();
+        long frameTime = newTime - currentSystemTime;
+        currentSystemTime = newTime;
+        accumulator += frameTime;
+        long deltaTime = 10000000;
+        while (accumulator >= deltaTime) {
+            //physics
+            System.out.println(accumulator);
+            accumulator -= deltaTime;
+            time += deltaTime;
+        }
+        //render
+        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.begin();
+        batch.draw(img, 0, 0);
+        batch.end();
+    }
 }
--- desktop/src/com/CheapCleaningAndCleaning/desktop/DesktopLauncher.java
+++ desktop/src/com/CheapCleaningAndCleaning/desktop/DesktopLauncher.java
@@ -1,12 +1,12 @@
 package com.CheapCleaningAndCleaning.desktop;
 
+import com.CheapCleaningAndCleaning.CheapCleaningAndCleaning;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.CheapCleaningAndCleaning.CheapCleaningAndCleaning;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new CheapCleaningAndCleaning(), config);
-	}
+    public static void main(String[] arg) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        new LwjglApplication(new CheapCleaningAndCleaning(), config);
+    }
 }
