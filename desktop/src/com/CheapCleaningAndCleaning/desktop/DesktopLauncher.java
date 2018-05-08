32ecfda64987c5d2405f1865b7a5a6d1a4d06f80
Artur Zubilewicz <artur.zubilewicz@gmail.com>
2018-05-08 16:16:24 +0200

    run the application in fullscreen mode

--- desktop/src/com/CheapCleaningAndCleaning/desktop/DesktopLauncher.java
+++ desktop/src/com/CheapCleaningAndCleaning/desktop/DesktopLauncher.java
@@ -7,6 +7,7 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 public class DesktopLauncher {
     public static void main(String[] arg) {
         LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.setFromDisplayMode(LwjglApplicationConfiguration.getDesktopDisplayMode());
         new LwjglApplication(new CheapCleaningAndCleaning(), config);
     }
 }
