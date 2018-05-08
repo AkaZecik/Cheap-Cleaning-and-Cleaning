6a6ffc0601d81211fd55801832377944c36f495e
Artur Zubilewicz <artur.zubilewicz@gmail.com>
2018-05-08 23:17:50 +0200

    Clear screen before drawing

--- core/src/com/CheapCleaningAndCleaning/ApplicationStates/AbstractApplicationState.java
+++ core/src/com/CheapCleaningAndCleaning/ApplicationStates/AbstractApplicationState.java
@@ -2,6 +2,7 @@ package com.CheapCleaningAndCleaning.ApplicationStates;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
@@ -16,6 +17,8 @@ public abstract class AbstractApplicationState implements ApplicationState {
 
     @Override
     public void render() {
+        Gdx.gl.glClearColor(1 ,1 , 1, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         stage.act();
         stage.draw();
     }
